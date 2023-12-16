dcf7c7b64c17609e0ae919d2da8819006b6b7e18
Saina <saina.firooz@gmail.com>
2023-12-16 11:26:24 +0100

    home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Nav from "../compartments/Nav";
+
+const HomePage = () => {
+
+    return (
+    <>
+    <h1>Habits & Tasks</h1>
+    <Nav />
+    
+    </>
+  )
+}
+
+export default HomePage;
