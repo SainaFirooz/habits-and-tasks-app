e0f87e7c7c406c12f32566f1fa336227f9b77c81
Saina <saina.firooz@gmail.com>
2023-12-20 00:55:19 +0100

    cleaner code

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import HomePage from "./pages/HomePage";
 import NewHabit from "./pages/NewHabit";
 
 const App = () => {
-  const storedHabits = JSON.parse(localStorage.getItem("habits")) || [];
-  const [habits, setHabits] = useState(storedHabits);
-  const storedStreaks = JSON.parse(localStorage.getItem("streaks")) || {};
-  const [streaks, setStreaks] = useState(storedStreaks);
+  const [habits, setHabits] = useState(() => JSON.parse(localStorage.getItem("habits")) || []);
+  const [streaks, setStreaks] = useState(() => JSON.parse(localStorage.getItem("streaks")) || []);
   const [topThreeHabits, setTopThreeHabits] = useState([]);
+
   const priorityOrder = { low: 1, medium: 2, high: 3 };
 
   const addHabit = (newHabit) => {
-    setHabits([...habits, { ...newHabit, streak: newHabit.startStreak }]);
+    setHabits((prevHabits) => [...prevHabits, { ...newHabit, streak: newHabit.startStreak }]);
   };
 
   useEffect(() => {
     const highPriority = habits.filter((habit) => habit.priority === "high");
-    
-    const sortedHabits = highPriority.sort((a, b) => priorityOrder[a.priority]);
-   
+    const sortedHabits = highPriority.sort((a, b) => priorityOrder[b.priority] - priorityOrder[a.priority]);
     const topThreeHabits = sortedHabits.slice(0, 3);
 
     setTopThreeHabits(topThreeHabits);
