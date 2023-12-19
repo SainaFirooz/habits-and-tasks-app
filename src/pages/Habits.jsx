34f7f203b4490889c208bf28f7d0d75a641a327f
Saina <saina.firooz@gmail.com>
2023-12-19 18:49:31 +0100

    useEffect

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -3,13 +3,21 @@ import { useNavigate } from "react-router-dom";
 import Nav from "../compartments/Nav";
 
 
-const Habits = () => {
+const Habits = ({ habits, setHabits}) => {
   const [filteredHabits, setFilteredHabits] = useState([]);
   const [streakSort, setStreakSort] = useState("highest to lowest");
   const [prioritySort, setPrioritySort] = useState("highest to lowest");
   const priorityOrder = { low: 1, medium: 2, high: 3};
   const [streaks, setStreaks] = useState(JSON.parse(localStorage.getItem("streaks")) || {});
   
+  useEffect(() => {
+    setFilteredHabits(habits);
+  }, [habits]);
+
+  useEffect(() => {
+    localStorage.setItem("habits", JSON.stringify(habits));
+    localStorage.setItem("streaks", JSON.stringify(streaks));
+  }, [habits, streaks]);
 
   return (
     <>
