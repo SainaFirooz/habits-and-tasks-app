19884bacc6f2c25c1e68f5c45053b4b1adf91839
Saina <saina.firooz@gmail.com>
2023-12-19 22:11:25 +0100

    decreaseStreak

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Nav from "../compartments/Nav";
 
-
-const Habits = ({ habits, setHabits}) => {
+const Habits = ({ habits, setHabits }) => {
   const [filteredHabits, setFilteredHabits] = useState([]);
   const [streakSort, setStreakSort] = useState("Sortera fallande");
   const [prioritySort, setPrioritySort] = useState("Sortera fallande");
-  const priorityValue = { low: 1, medium: 2, high: 3};
-  const [streaks, setStreaks] = useState(JSON.parse(localStorage.getItem("streaks")) || {});
-  
+  const priorityValue = { low: 1, medium: 2, high: 3 };
+  const [streaks, setStreaks] = useState(
+    JSON.parse(localStorage.getItem("streaks")) || {}
+  );
+
   useEffect(() => {
     setFilteredHabits(habits);
   }, [habits]);
@@ -19,12 +20,11 @@ const Habits = ({ habits, setHabits}) => {
     localStorage.setItem("streaks", JSON.stringify(streaks));
   }, [habits, streaks]);
 
-
   const handleSort = (sortByField, sortOrder) => {
     const sortHabits = [...filteredHabits].sort((a, b) => {
       let compareValueA, compareValueB;
 
-      if(sortByField === "streak") {
+      if (sortByField === "streak") {
         compareValueA = a.startStreak;
         compareValueB = b.startStreak;
       } else if (sortByField === "priority") {
@@ -44,42 +44,47 @@ const Habits = ({ habits, setHabits}) => {
   };
 
   const handleStreakSort = () => {
-    const streakOrder = 
-      streakSort === "Sortera fallande" ? "Sortera stigande" : "Sortera fallande";
+    const streakOrder =
+      streakSort === "Sortera fallande"
+        ? "Sortera stigande"
+        : "Sortera fallande";
     setStreakSort(streakOrder);
     handleSort("streak", streakOrder);
   };
 
-
   const handlePrioritySort = () => {
     const priorityOrder =
-     prioritySort ===  "Sortera fallande" ? "Sortera stigande" : "Sortera fallande";
-     setPrioritySort(priorityOrder);
-     handleSort("priority", priorityOrder);
+      prioritySort === "Sortera fallande"
+        ? "Sortera stigande"
+        : "Sortera fallande";
+    setPrioritySort(priorityOrder);
+    handleSort("priority", priorityOrder);
   };
 
   const increaseStreak = (habitTitle) => {
-    setHabits((prevHabits) => 
-    prevHabits.map((habit) => 
-      habit.title === habitTitle ? { ...habit, startStreak: (habit.startStreak || 0) + 1 } : habit
+    setHabits((prevHabits) =>
+      prevHabits.map((habit) =>
+        habit.title === habitTitle
+          ? { ...habit, startStreak: (habit.startStreak || 0) + 1 }
+          : habit
       )
     );
   };
 
+  const decreaseStreak = (habitTitle) => {
+    setHabits((prevHabits) =>
+      prevHabits.map((habit) =>
+        habit.title === habitTitle
+          ? {
+              ...habit,
+              startStreak: habit.startStreak > 0 ? habit.startStreak - 1 : 0,
+            }
+          : habit
+      )
+    );
+  };
 
-  
-
-
-
-
-
-
-
-  return (
-    <>
-    
-    </>
-  );
+  return <></>;
 };
 
 export default Habits;
