46fd47bf1522be3f19dd330ca186c78849a5b636
Saina <saina.firooz@gmail.com>
2023-12-19 17:26:21 +0100

    useStates

--- src/pages/NewHabit.jsx
+++ src/pages/NewHabit.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Nav from "../compartments/Nav";
+
 const NewHabit = () => {
+  const [title, setTitle] = useState("");
+  const [startStreak, setStartStreak] = useState(0);
+  const [priority, setPriority] = useState("low");
+
+
+
   return (
     <>
-      <h1>New Habits</h1>
+    <h1>Lägg till en ny vana</h1>
+    <Nav/>
+
     </>
-  );
+
+  )
+
 };
 
 export default NewHabit;
+
