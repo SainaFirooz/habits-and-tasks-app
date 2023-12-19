079a7c304868fdd8d1a5f0a0ac495d638093d1b5
Saina <saina.firooz@gmail.com>
2023-12-20 00:06:53 +0100

    top three habits map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,30 @@
 import Nav from "../compartments/Nav";
-import './style.css' 
-import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import "./style.css";
+import { Link } from "react-router-dom";
+
+const HomePage = ({ topThreeHabits }) => {
 
 
-const HomePage = () => {
-  const navigate = useNavigate();
-
-  const seeMorebtn = () => {
-    navigate("/habits");
-  }
 
   return (
     <>
-    <h2>Habits & Tasks</h2>
-    
-    
+      <h2>Habits & Tasks</h2>
+      <Nav />
+
+      <div>
+        <h2>Topp Tre Vanor</h2>
+        <div>
+          {topThreeHabits.map((habit) => (
+            <div key={habit.title}>
+              <h3>{habit.title}</h3>
+              <p>Prioritet{habit.priority}</p>
+            </div>
+          ))}
+          <Link to="/habits">Se fler vanor</Link>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
+
+export default HomePage;
