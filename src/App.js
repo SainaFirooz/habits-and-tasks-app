98589199f3c04c68b32e5586ea9311786fe588fa
Saina <saina.firooz@gmail.com>
2023-12-19 23:42:06 +0100

    storedStreaks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import NewHabit from "./pages/NewHabit";
 const App = () => {
   const storedHabits = JSON.parse(localStorage.getItem("habits")) || [];
   const [habits, setHabits] = useState(storedHabits);
+  const storedStreaks = JSON.parse(localStorage.getItem("streaks")) || {};
+  const [streaks, setStreaks] = useState(storedHabits);
   
+
 
   return (
     <div>
