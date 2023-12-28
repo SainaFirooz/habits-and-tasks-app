0aa4acf79de6023fb9e62c55722ba19b4ae3ae09
broder-robert <robertluigithompson@gmail.com>
2023-12-28 19:13:34 +0100

    Ändrat svensk text till engelska

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -41,29 +41,29 @@ const NewTask = () => {
                         type="text"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Titel"
+                        placeholder="Title"
                     />
                     <textarea className="w-72 border-2 rounded-md px-3 py-3 bg-[#E8ECF4] backdrop-blur-lg"
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Beskrivning"
+                        placeholder="Description"
                     />
                     <input className="w-72 border-2 rounded-md px-3 py-3 bg-[#E8ECF4] backdrop-blur-lg"
                         type="text"
                         value={estimate}
                         onChange={(e) => setEstimate(e.target.value)}
-                        placeholder="Tidsestimat"
+                        placeholder="Time estimate"
                     />
                     <select value={type} onChange={(e) => setType(e.target.value)} className="w-72 border-2 rounded-md px-3 py-3 bg-[#E8ECF4] backdrop-blur-lg">
-                        <option value="">Välj typ av ärende</option>
-                        <option value="hushållssysslor">Hushållssysslor</option>
-                        <option value="aktivitet med vänner">Aktivitet med vänner</option>
-                        <option value="jobbrelaterad">Jobbrelaterad</option>
+                        <option value="">Select the type of task</option>
+                        <option value="hushållssysslor">Household chores</option>
+                        <option value="aktivitet med vänner">Activity with friends</option>
+                        <option value="jobbrelaterad">Job related</option>
                     </select>
                 </div>
                 <div className="flex gap-4 justify-center w-72">
-                    <button className="flex-grow px-5 py-2 bg-sky-900 text-white font-medium rounded-md" type="button" onClick={fetchActivity}>Hämta förslag på aktivitet</button>
-                    <button className="flex-grow px-5 py-2 bg-sky-900 text-white font-medium rounded-md" type="submit">Skapa ärende</button>
+                    <button className="flex-grow px-5 py-2 bg-sky-900 text-white font-medium rounded-md" type="button" onClick={fetchActivity}>Fetch suggestions for activities</button>
+                    <button className="flex-grow px-5 py-2 bg-sky-900 text-white font-medium rounded-md" type="submit">Create task</button>
                 </div>
             </form>
         </div>
